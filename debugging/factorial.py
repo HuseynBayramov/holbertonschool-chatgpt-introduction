fd830a216d45f3e7ff924a6b52472aca8c5dbb90
holbertonhuseyn <13869@holbertonstudents.com>
2026-04-30 15:53:10 +0400

    Update factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+
 def factorial(n):
     result = 1
     while n > 1:
@@ -7,5 +8,12 @@ def factorial(n):
         n -= 1
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+# Əsas hissə
+if len(sys.argv) > 1:
+    try:
+        num = int(sys.argv[1])
+        print(factorial(num))
+    except:
+        print(0)
+else:
+    print(0)
